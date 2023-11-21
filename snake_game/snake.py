b43296bdfbee00be4d9a8bc759e9fd14ff6853b0
sidmaroli <119603972+sidmaroli@users.noreply.github.com>
2023-11-21 17:40:12 -0300

    grd

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,40 +1,27 @@
-from math import gamma
-from sys import flags
-from tkinter import font
 import pygame
-import time
 import random
 
 
-
-WIDTH = 800
-HEIGTH = 600
+WIDTH = 700
+HEIGTH = 400
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
-game_over = False
 
 white = (225,225,225)
-black = (0,0,0)
+green = (0,128,0)
 blue = (0,0,225)
 red = (225,0,0)
+black = (0,0,0)
 
 
 snake_block = 10
-
+tickt = 15
 
 clock = pygame.time.Clock()
 snake_speed = 10
-
-font_style = pygame.font.SysFont(None, 50)
-
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    screen.blit(mesg, (WIDTH/2, HEIGTH/2))
-
-
 
 
 game_over = False
@@ -51,19 +38,10 @@ foody = round(random.randrange(0, WIDTH - snake_block) / 10.0) * 10.0
 
 while not game_over:
 
-    #while game_close == True:
-    #    dis.fill(white)
-    #    message("Você perdeu! Pressione Q-quit ou C-jogar de novo", red)
-    #    pygame.display.update()
-
-        #for event.type in pygame.event.get():
-            #if event.type == pygame.KEYDOWN:
-                # if event.key == pygame.K_q:
-                #    game_close = False
-                #    game_over = True
-                #if event.key == pygame.K_c:
-                    #gameLoop()
-                    
+    while game_close == True:
+        screen.fill(white)
+        pygame.display.update()
+ 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -86,14 +64,12 @@ while not game_over:
 
     x1 += x1_change
     y1 += y1_change
-    screen.fill(white)
+    screen.fill(black)
 
-    #pygame.draw.rect(screen, blue,[foodx, foody, snake_block, snake_block])
-    pygame.draw.rect(screen, black,[x1, y1, snake_block, snake_block])
+    pygame.draw.rect(screen, red,[foodx, foody, snake_block, snake_block])
+    pygame.draw.rect(screen, green,[x1, y1, snake_block, snake_block])
 
-    clock.tick(snake_block)
-
-
-    #message("Você perdeu", red)    
+    clock.tick(tickt)
+ 
     pygame.display.update()        
-    time.sleep(2)
+
